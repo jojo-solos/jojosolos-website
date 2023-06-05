3dda9ecf42cd2078fda2ca4748915b1a700197cc
jojo-solos <beatsolos.b@gmail.com>
2023-06-04 22:42:28 -0500

    jojosolos website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import  Typed  from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'jojosolos-website';
+  image:any;
+  link:any;
+  intro:any;
+
+  insideHeight:any;
+  insideWidth:any;
+
+  clicks:any;
+
+  constructor() {
+
+  }
+
+  ngOnInit () {
+    var typed = new Typed(".auto-type", {
+      strings: ["meow", "meOw"],
+      typeSpeed: 150,
+      backSpeed: 200,
+      loop: true
+    });
+
+    var typed2 = new Typed(".auto-type-link", {
+      strings: ["jojosolos", "bea", "joj0"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true
+    });
+
+    this.image = document.getElementById("myImage");
+    this.link = document.getElementById("myLink");
+    this.intro = document.getElementById("myIntro");
+    this.link.style.visibility = 'hidden';
+
+    this.moveImage();
+
+    this.insideHeight = window.innerHeight;
+    this.insideWidth = window.innerWidth;
+
+    this.clicks = 0;
+
+    console.log("height: ", this.insideHeight, " width: ", this.insideWidth);
+  }
+
+  test() {
+    console.log("test");
+  }
+
+  moveImage() {
+    console.log("clicked on image");
+    if(this.clicks >= 10) {
+      window.open("https://www.twitch.tv/jojosolos");
+      this.image.style.visibility = 'hidden';
+      this.intro.style.visibility = 'hidden';
+      this.link.style.visibility = 'visible';
+    }
+
+    var x = Math.floor(Math.random() * this.insideWidth) - 75;
+    var y = Math.floor(Math.random() * this.insideHeight) - 120;
+
+    console.log("x: ", x, " y: ", y);
+
+    if(this.image != null) {
+      this.image.style.top =  y + "px";
+      this.image.style.left = x + "px";
+    }
+    this.clicks++;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { innerWidth: any; }; }) {
+    console.log("RESIZE!!");
+
+    this.insideHeight = window.innerHeight;
+    this.insideWidth = window.innerWidth;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '**', component: AppComponent}
+    ]),
     AppRoutingModule
   ],
   providers: [],
